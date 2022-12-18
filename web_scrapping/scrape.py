dc75a571286dff4a238f5bd48bdd03e324e6dede
Qusay <qusayfadhel@gmail.com>
2022-12-18 23:47:54 +0200

    Fix bugs - Fixes parse_content(content) - Fixes save_data(data, columns)

--- web_scrapping/scrape.py
+++ web_scrapping/scrape.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from csv import writer
 import pandas as pd
+import os
 
 
 def save_data(data, columns):
@@ -16,19 +17,26 @@ def save_data(data, columns):
     """
 
     # Create a dataframe using the passed dataset, and save it to a csv file on the disk
+    print("saving data...")
     df = pd.DataFrame(data, columns=columns)
-    df.to_csv("output/housing.csv")
+    # Check if the output folder exists and if not create it and form the full path
+    file_name = 'housing.csv'
+    outdir = './output'
+    if not os.path.exists(outdir):
+       os.mkdir(outdir)
+    full_path = os.path.join(outdir, file_name)
+    df.to_csv(full_path, encoding='utf-8')
     return df
 
 
 def scrape_page(url, page_number):
     """
-    Scrapes the speified page of the passed URL (i.e., paginated scrapping)
+    Scrapes the speified page of the passed URL (i.e., handles paginated scrapping).
 
     Args:
-        url: The url of the website from which data will be scrapped (i.e. https://housinganywhere.com/s/Berlin--Germany).
-        This URL is to be combined with page_number during execution.
-        page_number: The number of the page to be scrapped from the specified URL
+        url: The url of the website from which data will be scrapped (i.e. https://housinganywhere.com/s/Berlin--Germany)
+        This URL is to be combined with page_number during execution
+        page_number: The number of the page to be scrapped from the specified URL. The website displays its data in pages where user navigates through them through the provided pager, where page_number is passed with the URL in a query string to retrieve the corresponding page data
     Returns:
         A bytes array representing extracted page content
 
@@ -45,11 +53,11 @@ def scrape_pages(url, start_page, end_page):
     """
     Scrapes multiple pages from a website all at once.
     Args:
-        url: The url of the website from which data will be scrapped (i.e. https://housinganywhere.com/s/Berlin--Germany). To be combined with the page number during execution.
+        url: The url of the website from which data will be scrapped (i.e. https://housinganywhere.com/s/Berlin--Germany). To be combined with the page number during execution
         start_page: The page index at which the application will start data extraction
-        end_page: The page index at which the application will stop data extraction
+        end_page: The page index at which the application will stop data extraction (inclusive)
     Returns:
-        A list of extracted content. Each item in this list represents a bytes array of the content extractced from one page.
+        A list of extracted content. Each item in this list represents a bytes array of the content extractced from one page
     """
 
     # Validate passed arguments
@@ -67,15 +75,15 @@ def scrape_pages(url, start_page, end_page):
 def parse_content(content):
     """
     Args:
-        content: A list of content extracted from the specified pages.
+        content: A list of content extracted from the specified pages
         Each item in this list represents the data extracted from one page
     Returns:
-        A list of parsed elements. Each item in this list represents the elements tags extractced from one page.
+        A list of parsed elements. Each item in this list represents the elements tags extractced from one page
     """
 
+    parsed_data = []
     if content is not None:
         for item in content:
-            parsed_data = []
             soup = BeautifulSoup(item, "html.parser")
 
             # The root section that encompasses all the data elements we are interested in
@@ -110,11 +118,11 @@ def parse_content(content):
 
 def process_pages(url, start_page, end_page):
     """
-    Abstracts the data extraction and parsing logic, as well as user interface updates
+    Abstracts the data extraction and parsing logic, as well as user interface updates.
     Args:
-        url: The url of the website from which data will be scrapped (i.e. https://housinganywhere.com/s/Berlin--Germany). To be combined with the page number during execution.
+        url: The url of the website from which data will be scrapped (i.e. https://housinganywhere.com/s/Berlin--Germany). To be combined with the page number during execution
         start_page: The page index at which the application will start data extraction
-        end_page: The page index at which the application will stop data extraction
+        end_page: The page index at which the application will stop data extraction (inclusive)
     Returns:
         None
     """
@@ -126,29 +134,30 @@ def process_pages(url, start_page, end_page):
         print("Parsing extracted content...")
         data = parse_content(pages_content)
         if data is not None:
-            print("saving data...")
             columns = ["Id", "URL", "Price", "Specs", "Availability"]
             df = save_data(data, columns)
             if df is not None:
-                print("Data scrapping completed successfully.")
-                print(f"Number of extracted properties: {len(df)}")
+                print("Data scrapping completed successfully")
+                print(f"Number of processed properties: {len(df)}")
                 print("Printing sample data")
                 print("*" * 150)
                 print(df.head(5))
             else:
-                print("Could not save data.")
+                print("Could not save data")
         else:
-            print("Could not parse content.")
+            print("Could not parse content")
     else:
-        print("Could not extract page data.")
+        print("Could not extract page data")
 
 
 def main():
+
     url = "https://housinganywhere.com/s/Berlin--Germany"
-
-    # Process pages 3-7 of the aforementioned URL
-    process_pages(url, 3, 7)
-
+    try:
+        # Process pages 11-20 of the aforementioned URL
+        process_pages(url, 11, 20)
+    except Exception as ex:
+        print(f"Something went wrong!\n{ex}")
 
 if __name__ == "__main__":
     main()
